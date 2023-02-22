92a7f5ababe2c44b06786658ca39f7a62c94188b
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-22 13:59:59 -0800

    init commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from pc_builder_app.extensions import app, db
+from pc_builder_app.main.routes import main
+from pc_builder_app.auth.routes import auth
+
+app.register_blueprint(main)
+app.register_blueprint(auth)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ pc_builder_app/extensions.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from pc_builder_app.config import Config
+import os
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+###########################
+# Authentication
+###########################
+
+# TODO: Add authentication setup code here!
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
+login_manager.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
