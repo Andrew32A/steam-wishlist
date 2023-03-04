cfcdff4b34cf44cc29f1357ab47cfaababd0db4a
Andrew Alsing <andrew32alsing@gmail.com>
2023-03-04 15:50:28 -0800

    added delete game button and styling

--- steam_wishlist_app/main/routes.py
+++ steam_wishlist_app/main/routes.py
@@ -8,6 +8,7 @@ from steam_wishlist_app.extensions import db, bcrypt
 
 main = Blueprint("main", __name__)
 
+# helper function to seed database
 def init_db():
     db.drop_all()
     db.create_all()
@@ -148,6 +149,7 @@ def init_db():
 
 init_db()
 
+# landing page that displays all games and users
 @main.route('/')
 def homepage():
     all_games = Game.query.all()
@@ -155,13 +157,12 @@ def homepage():
     return render_template('home.html',
         all_games=all_games, all_users=all_users)
 
-
+# create games
 @main.route('/create_game', methods=['GET', 'POST'])
 @login_required
 def create_game():
     form = GameForm()
 
-    # if form was submitted and contained no errors
     if form.validate_on_submit(): 
         new_game = Game(
             title=form.title.data,
@@ -176,11 +177,12 @@ def create_game():
         return redirect(url_for('main.game_detail', game_id=new_game.id))
     return render_template('create_game.html', form=form)
 
-
+# create publishers
 @main.route('/create_publisher', methods=['GET', 'POST'])
 @login_required
 def create_author():
     form = AuthorForm()
+
     if form.validate_on_submit():
         new_publisher = Publisher(
             name=form.name.data
@@ -190,8 +192,6 @@ def create_author():
 
         flash('New publisher was added successfully.')
         return redirect(url_for('main.homepage'))
-    
-    # if form was not valid, or was not submitted yet
     return render_template('create_publisher.html', form=form)
 
 @main.route('/game/<game_id>', methods=['GET', 'POST'])
@@ -199,7 +199,6 @@ def game_detail(game_id):
     game = Game.query.get(game_id)
     form = GameForm(obj=game)
     
-    # if form was submitted and contained no errors
     if form.validate_on_submit():
         game.title = form.title.data
         game.image = form.image.data
@@ -213,7 +212,7 @@ def game_detail(game_id):
 
     return render_template('game_detail.html', game=game, form=form)
 
-
+# view user details
 @main.route('/profile/<username>')
 def profile(username):
     all_games = Game.query.all()
@@ -223,11 +222,12 @@ def profile(username):
 
     return render_template('profile.html', user=user, all_games=all_games, user_wishlist=user_wishlist)
 
-
+# wishlist game
 @main.route('/favorite/<game_id>', methods=['POST'])
 @login_required
 def favorite_game(game_id):
     game = Game.query.get(game_id)
+
     if game in current_user.favorite_games:
         flash('Game already in wishlist.')
     else:
@@ -237,11 +237,12 @@ def favorite_game(game_id):
         flash('Game added to wishlist.')
     return redirect(url_for('main.game_detail', game_id=game_id))
 
-
+# unwishlist game
 @main.route('/unfavorite/<game_id>', methods=['POST'])
 @login_required
 def unfavorite_game(game_id):
     game = Game.query.get(game_id)
+
     if game not in current_user.favorite_games:
         flash('Game not in wishlist.')
     else:
@@ -250,3 +251,13 @@ def unfavorite_game(game_id):
         db.session.commit()
         flash('Game removed from wishlist.')
     return redirect(url_for('main.game_detail', game_id=game_id))
+
+# delete game
+@main.route('/delete_game/<game_id>', methods=['POST', 'GET'])
+@login_required
+def delete_game(game_id):
+    game = Game.query.filter_by(id=game_id).one()
+
+    db.session.delete(game)
+    db.session.commit()
+    return redirect(url_for('main.homepage'))
